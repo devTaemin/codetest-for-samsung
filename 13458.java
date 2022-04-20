0506102fbe53c85c57834670733b7dac68fcf046
Taemin Im <devTaemin@gmail.com>
2022-04-21 05:05:26 +0900

    Create 13458.java

---
+++ 13458.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+class Main {
+	public static long count;
+	public static int N, B, C;
+	public static int[] rooms;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		rooms = new int[N + 1];
+		
+		for (int i = 1; i <= N; i++) {
+			rooms[i] = sc.nextInt();
+		}
+		
+		B = sc.nextInt();
+		C = sc.nextInt();
+		
+		Inspector();
+		sc.close();
+	}
+	
+	public static void Inspector() {
+		
+		for (int i = 1; i <= N; i++) {
+			// rooms[i] >= 1
+			if (rooms[i] < B) { rooms[i] = 0; }
+			else { rooms[i] -= B; }
+			count++;
+		}
+		
+		for (int i = 1; i <= N; i++) {
+			if (rooms[i] != 0) {
+				int div = rooms[i] / C;
+				int rest = rooms[i] % C;
+				
+				if (rest != 0) div += 1;
+				
+				count += div;
+			}
+		}
+
+		System.out.println(count);
+	}
+}
